cb72039ee2f7eb57f20e054ec0d493075d8f1c94
“Harsh1207” <gosalia@ualberta.ca>
2023-02-02 19:33:53 -0700

    i added the triangle

--- app/src/main/java/com/example/notconstraintlayout/Shape.java
+++ app/src/main/java/com/example/notconstraintlayout/Shape.java
@@ -2,9 +2,6 @@ package com.example.notconstraintlayout;
 
 public abstract class Shape {
     private int x, y;
-<<<<<<< HEAD
-    String color = "blue";
-=======
+
     String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> 09e761eefa72b6f208aad9dda9d44ae9eb17346e
 }
